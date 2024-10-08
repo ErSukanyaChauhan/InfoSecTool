b7e2b2370862d998a5d6bc5858318375da00e058
ErSukanya Chauhan <151927764+ErSukanyaChauhan@users.noreply.github.com>
2024-10-08 21:07:46 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,11 +7,21 @@ const app = express();
 app.use(cors()); // Enable CORS for all routes
 // Serve the static files from the React app
 
-app.get('/', (req, res) => {
-  res.send('HASH is working!');
+// app.get('/', (req, res) => {
+//   res.send('HASH is working!');
+// });
+
+app.get('/api/hash/:hash', (req, res) => {
+  const hash = req.params.hash;
+  const options = {
+    url: `http://www.virustotal.com/api/v3/files/${hash}`,
+    headers: {
+      accept: 'application/json',
+      'x-apikey': '4a812c43e5c6d22664cba618b14df7830dac5979759fa6b447ac9d3a6b8a22cd'
+    }
+  };
+  request(options).pipe(res);
 });
-
-
 
 const PORT = 5000;
 app.listen(PORT, () => {
