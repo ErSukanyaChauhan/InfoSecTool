1e8fe6b3146f2663127afddcd8be6ad1e89540cd
ErSukanya Chauhan <151927764+ErSukanyaChauhan@users.noreply.github.com>
2024-10-08 13:08:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,22 +8,23 @@ app.use(cors()); // Enable CORS for all routes
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
-// app.get('/api/hash/:has', (req, res) => {
-//   const hash = req.params.hash;
-//   const options = {
-//     url: `http://www.virustotal.com/api/v3/files/${hash}`,
-//     headers: {
-//       accept: 'application/json',
-//       'x-apikey': '4a812c43e5c6d22664cba618b14df7830dac5979759fa6b447ac9d3a6b8a22cd'
-//     }
-//   };
-//   request(options).pipe(res);
-// });
+app.get('/', (req, res) => {
+  const hash = req.params.hash;
+  const options = {
+    url: `http://www.virustotal.com/api/v3/files/${hash}`,
+    headers: {
+      accept: 'application/json',
+      'x-apikey': '4a812c43e5c6d22664cba618b14df7830dac5979759fa6b447ac9d3a6b8a22cd'
+    }
+  };
+  res.send('Hello, from server side!');
+  request(options).pipe(res);
+});
 
 // Handles any requests that don't match the ones above
-app.get('/', (req, res) => {
-  res.send('Hello, from server side!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello, from server side!');
+// });
 
 const PORT = 4000;
 app.listen(PORT, () => {
